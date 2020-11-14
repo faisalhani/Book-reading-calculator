00348142ab973759fa7afeb984fc27e7fc9b2d35
faisalhani <faisal.hani.1996@taibahu.edu.sa>
2020-11-14 23:04:45 +0300

    new

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Calculator from './components/Calculator'
+function App() {
+  return (<div className="container">
+    <Calculator />
+
+  </div>);
+}
+
+export default App;
