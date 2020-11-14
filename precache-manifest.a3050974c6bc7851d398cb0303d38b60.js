458b5ea774333d602d6eefc58f4ee831763c3a3a
faisalhani <faisal.hani.1996@taibahu.edu.sa>
2020-11-14 23:14:31 +0300

    Updates

---
+++ precache-manifest.a3050974c6bc7851d398cb0303d38b60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e4dc0e6016abdbafac6ce7e9a2dc5d",
+    "url": "/Book-reading-calculator/index.html"
+  },
+  {
+    "revision": "7e83acc8a1fce0543c0c",
+    "url": "/Book-reading-calculator/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "48f188a38d1a3d93c8e1",
+    "url": "/Book-reading-calculator/static/css/main.438c066b.chunk.css"
+  },
+  {
+    "revision": "7e83acc8a1fce0543c0c",
+    "url": "/Book-reading-calculator/static/js/2.938e02fb.chunk.js"
+  },
+  {
+    "revision": "48f188a38d1a3d93c8e1",
+    "url": "/Book-reading-calculator/static/js/main.72d93725.chunk.js"
+  },
+  {
+    "revision": "30e01f9f2305b4120f02",
+    "url": "/Book-reading-calculator/static/js/runtime-main.3ab526d9.js"
+  }
+]);
